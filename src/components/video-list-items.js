4cdd79875dd410340e2a7538ebc65a9ecb461b2f
Bhavika Bhalani <bbhalani@albany.edu>
2018-04-12 16:27:23 -0400

    Revert "Revert "Revert "React section 3 complete"""
    This reverts commit 5dc48321221358f7631cccd90e778f368a471f87.

--- src/components/video-list-items.js
+++ src/components/video-list-items.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 //destructuring
-const VideoListItem = ({video, onVideoSelect}) => {
+const VideoListItem = ({video}) => {
 
     const imageUrl = video.snippet.thumbnails.default.url;
     const videoTitle = video.snippet.title;
     return (
-        <li onClick={()=>onVideoSelect(video)} className="list-group-item">
+        <li className="list-group-item">
             <div className="media video-list">
                 <div className="media-left">
                     <img className="media-object" src={imageUrl} />
